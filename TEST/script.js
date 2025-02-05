c41e96fba2cfee3ea0b6e6d9f4539557f4501305
arya <aryaadiguna12@gmail.com>
2025-02-05 21:38:23 +0800

    raul
    tes

--- TEST/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  function toggleMenu() {
-      var menu = document.getElementById("mobile-menu");
-      menu.classList.toggle("active"); // Menggunakan kelas active
-  }
-
-  document.querySelector(".mobile-menu-button").addEventListener("click", toggleMenu);
-});
